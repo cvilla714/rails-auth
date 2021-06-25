222f90d9c286ae1a2de101b193a17426f99910c8
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-25 18:40:39 -0400

    added the log in and log out methods and imported the CurrentUserConcern

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
   def create
     user = User
            .find_by(email: params['user']['email'])
@@ -13,6 +14,23 @@ class SessionsController < ApplicationController
       }
     else
       render json: { status: 401 }
+    end
+    
+    def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user:@current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+
+    def logout
+    reset_session
+    render json: {status: 200, logged_out: true}
     end
   end
 end
