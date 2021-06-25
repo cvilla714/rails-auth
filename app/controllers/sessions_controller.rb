b1fab60623255a93cbd40583b2169c192dc035a6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-25 01:59:42 -0400

    built the session_controller and added the create methosd

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+    user = User
+           .find_by(emai: params['user']['email'])
+           .try(:authenticate, params['user']['password'])
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
+    end
+  end
+end
