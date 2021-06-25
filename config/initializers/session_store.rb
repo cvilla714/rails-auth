de6367199e0296f7ce26ee9acfd5b4e1c5ecf216
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-25 18:50:56 -0400

    checking if rails is ujsing production or developing mode for the app to be able to be accesable on both environments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,7 @@
-Rails.application.config.session_store :cookie_store, key: '_authentication_app',
-                                                      domain: 'lavista-authentication-api.herokuapp.com'
+if Rails.env == 'production'
+
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app',
+                                                        domain: 'lavista-authentication-api.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+end
