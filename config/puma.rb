a44d54a083826b00795d9079e27bd5197d8d5829
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-26 16:52:04 -0400

    added new configuration to puma to find out the next available port to render the content

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,17 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
+
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#If default is in use, increment the default port number by 1 until you hit an open port
+#
+new_port = 3000
+while system("lsof -i:#{new_port}")
+  new_port += 1
+end
+port        ENV.fetch("PORT") { new_port }
+
 
 # Specifies the `environment` that Puma will run in.
 #
