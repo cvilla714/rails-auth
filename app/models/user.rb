0e86b355477b284b710addf84fa9714aa41a8b4f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-17 18:09:58 -0400

    added name to be presence when creating the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :expenses
 
-  validates :email, presence: true
+  validates :email, :name, presence: true
   validates :email, uniqueness: true
 end
