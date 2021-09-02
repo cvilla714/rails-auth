c79f9a5749be6e420c58e85d7b1f7f533e905cc8
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-01 23:18:43 -0400

    Added correct app link for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   
   
   allow do
-    origins 'https://vibrant-heyrovsky-039c58.netlify.app/'
+    origins 'https://lavista-authentication-api.herokuapp.com/'
 
     resource '*',
              headers: :any,
