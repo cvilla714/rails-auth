783a25499cf52b4f800eaf9b53348b65c6d04910
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 14:54:45 -0400

    correcting the domain address to allow store cookies in the browser

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 if Rails.env == 'production'
 
   Rails.application.config.session_store :cookie_store, key: '_authentication_app',
-                                                        domain: 'https://lavista-authentication-api.herokuapp.com'
+                                                        domain: 'lavista-authentication-api.herokuapp.com'
                                                         
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
