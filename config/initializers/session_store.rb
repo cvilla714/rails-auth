9b9a939de8afd10e32e065d328cd6aea536eb2f3
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 16:37:50 -0400

    added SameSite to none and secure to allow store the cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,8 @@
 if Rails.env == 'production'
 
   Rails.application.config.session_store :cookie_store, key: '_authentication_app',
-                                                        domain: 'lavista-authentication-api.herokuapp.com'
+                                                        domain: 'lavista-authentication-api.herokuapp.com',
+                                                        SameSite: None; Secure
                                                         
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
