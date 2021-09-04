adc5942e712009f714b6de9f745e486fa6e3b027
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 16:36:30 -0400

    added find_by to avoid app from crashing

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,6 +6,6 @@ module CurrentUserConcern
   end
 
   def set_current_user
-    @current_user = User.find(session[:user_id]) if session[:user_id]
+    @current_user = User.find_by(session[:user_id]) if session[:user_id]
   end
 end
