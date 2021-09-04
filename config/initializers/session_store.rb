587426d38974282dceac46dc0291d1224c162e1b
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 16:42:54 -0400

    wrapped noen and secure into quotes to avoid problems with deploying the add

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,8 +2,7 @@ if Rails.env == 'production'
 
   Rails.application.config.session_store :cookie_store, key: '_authentication_app',
                                                         domain: 'lavista-authentication-api.herokuapp.com',
-                                                        SameSite: None; Secure
-                                                        
+                                                        SameSite: 'None'; 'Secure'
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
