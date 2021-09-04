fb9854b3318e66148299397d5d00f2f51c89a97b
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 15:01:02 -0400

    changing domain name to allow store cookie in browser

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 if Rails.env == 'production'
 
   Rails.application.config.session_store :cookie_store, key: '_authentication_app',
-                                                        domain: 'lavista-authentication-api.herokuapp.com'
+                                                        domain: 'vibrant-heyrovsky-039c58.netlify.app'
                                                         
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
