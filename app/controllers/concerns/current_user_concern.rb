1f0a30ed2c008bd84f3424fe048e9dbbc789a052
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 22:00:45 -0400

    got ride of old code

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,11 +1,11 @@
-module CurrentUserConcern
-  extend ActiveSupport::Concern
+# module CurrentUserConcern
+#   extend ActiveSupport::Concern
 
-  included do
-    before_action :set_current_user
-  end
+#   included do
+#     before_action :set_current_user
+#   end
 
-  def set_current_user
-    @current_user = User.find_by(session[:user_id]) if session[:user_id]
-  end
-end
+#   def set_current_user
+#     @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+#   end
+# end
