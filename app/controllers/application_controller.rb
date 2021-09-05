b0e4b67f60e9398fc384ac203a8a87c74fae64d4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 22:00:12 -0400

    added the method set current_user in here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
+  before_action :set_current_user
+
+  def set_current_user
+    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
