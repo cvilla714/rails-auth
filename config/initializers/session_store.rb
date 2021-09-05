16e2d84abe277a59da359fa7999f73bbf6c60ca5
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 23:00:22 -0400

    deleted previous method to store the session

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env == 'production'
-
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app',
-                                                        domain: 'lavista-authentication-api.herokuapp.com',
-                                                        SameSite: 'None'; 'Secure'
-else
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
-end
