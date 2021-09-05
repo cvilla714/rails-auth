e6397d06d82b3926b61204b10e2e06dfe3dddfcf
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 22:01:25 -0400

    added private method and new instance variables to call the user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,43 @@
 class RegistrationsController < ApplicationController
-  def create
-    user = User.create!(
-      name: params['user']['name'],
-      email: params['user']['email'],
-      password: params['user']['password'],
-      password_confirmation: params['user']['password_confirmation']
-    )
+  def new
+    @user = User.new
+  end
 
-    if user
+  def create
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       render json: {
         status: :created,
-        user: user
+        user: @user
       }
     else
       render json: { status: 500 }
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
+
+# def create
+#   user = User.create!(
+#     name: params['user']['name'],
+#     email: params['user']['email'],
+#     password: params['user']['password'],
+#     password_confirmation: params['user']['password_confirmation']
+#   )
+
+#   if user
+#     session[:user_id] = user.id
+#     render json: {
+#       status: :created,
+#       user: user
+#     }
+#   else
+#     render json: { status: 500 }
+#   end
+# end
