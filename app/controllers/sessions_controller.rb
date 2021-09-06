8d6493b2758ef83e231857278fd1e3bacf7c0802
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-06 17:49:14 -0400

    excluded the CurrentUserConcern method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  include CurrentUserConcern
+  # include CurrentUserConcern
   def create
     user = User
       .find_by(email: params['user']['email'])
