7e5234f794bcb02b7dfe9c981e3560b49a3c68be
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-06 17:56:33 -0400

    Deleted code that wasn't been used anymore

--- config/initializers/session_store.rb
+++
@@ -1,7 +0,0 @@
-# if Rails.env == 'production'
-
-#     Rails.application.config.session_store :cookie_store, key: '_authentication_app',
-#                                                           domain: 'lavista-authentication-api.herokuapp.com'
-#   else
-#     Rails.application.config.session_store :cookie_store, key: '_authentication_app'
-#   end
