f12e420971d3b97b0cf9ba75880bc866ee0ee0e6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-06 13:08:16 -0400

    added to cors any request

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,18 +7,19 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    # origins 'http://localhost:3000'
+    origins '*'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
              credentials: true
   end
   
-  allow do
-    origins 'https://vibrant-heyrovsky-039c58.netlify.app'
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true
-  end
+  # allow do
+  #   origins 'https://vibrant-heyrovsky-039c58.netlify.app'
+  #   resource '*',
+  #            headers: :any,
+  #            methods: %i[get post put patch delete options head],
+  #            credentials: true
+  # end
 end
