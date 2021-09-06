f3312d91ed2244ed8931d5a1694fc99d86c25523
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-06 17:40:11 -0400

    Use the previous method to register a user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,44 +1,44 @@
 class RegistrationsController < ApplicationController
-  #   def new
-  #     @user = User.new
-  #   end
-
-  #   def create
-  #     @user = User.new(user_params)
-  #     if @user.save
-  #       session[:user_id] = user.id
-  #       render json: {
-  #         status: :created,
-  #         user: @user
-  #       }
-  #     else
-  #       render json: { status: 500 }
-  #     end
-  #   end
-
-  #   private
-
-  #   def user_params
-  #     params.require(:user).permit(:email, :password, :password_confirmation)
-  #   end
-  # end
+  def new
+    @user = User.new
+  end
 
   def create
-    user = User.create!(
-      name: params['user']['name'],
-      email: params['user']['email'],
-      password: params['user']['password'],
-      password_confirmation: params['user']['password_confirmation']
-    )
-
-    if user
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       render json: {
         status: :created,
-        user: user
+        user: @user
       }
     else
       render json: { status: 500 }
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
+
+# def create
+#   user = User.create!(
+#     name: params['user']['name'],
+#     email: params['user']['email'],
+#     password: params['user']['password'],
+#     password_confirmation: params['user']['password_confirmation']
+#   )
+
+#   if user
+#     session[:user_id] = user.id
+#     render json: {
+#       status: :created,
+#       user: user
+#     }
+#   else
+#     render json: { status: 500 }
+#   end
+# end
+# end
