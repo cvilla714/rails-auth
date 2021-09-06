326c5420de0dca18153f56dbcdbc8fdef3ba410f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-06 17:39:00 -0400

    Put back the configuration for calling set_current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   include ActionController::Cookies
 
-  # before_action :set_current_user
+  before_action :set_current_user
 
-  # def set_current_user
-  #   Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
-  # end
+  def set_current_user
+    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
