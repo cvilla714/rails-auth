e600b37f1e55c4dc940977df556b1e2cb9aa22c0
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-06 13:12:20 -0400

    changed credentials from true to false

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
-             credentials: true
+             credentials: false
   end
   
   # allow do
