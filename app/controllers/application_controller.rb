b25e3812f54fea0124214d71a9b8825ef3b5d806
Cosmel Villalobos <cosmelvg@gmail.com>
2021-10-03 19:17:39 -0400

    Modified the wayt to set the current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
 
   def set_current_user
-    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+    # Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user = User.find_by(session[:user_id]) if session[:user_id]
   end
 end
