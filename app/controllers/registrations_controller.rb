b6591a4ad6f5f0c7eb4983f40646d26e8a2cf157
Cosmel Villalobos <cosmelvg@gmail.com>
2021-10-03 19:18:10 -0400

    Changed the way to register a new user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,24 +1,31 @@
 class RegistrationsController < ApplicationController
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
+    # @user = User.new(user_params)
+    user = User.create!(
+      name: params['user']['name'],
+      email: params['user']['email'],
+      password: params['user']['password'],
+      password_confirmation: params['user']['password_confirmation']
+    )
+
+    if user
       session[:user_id] = user.id
       render json: {
         status: :created,
-        user: @user
+        user: user
       }
     else
       render json: { status: 500 }
     end
   end
 
-  private
+  # private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
+  # def user_params
+  #   params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  # end
 end
