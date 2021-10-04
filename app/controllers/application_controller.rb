ec0c7ff37b77c99d9e80e9c6d9afd5a99d669b79
Cosmel Villalobos <cosmelvg@gmail.com>
2021-10-03 20:01:46 -0400

    Changed the way to find the current user using the session of the user id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def set_current_user
     # Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
-    @current_user = User.find_by(session[:user_id]) if session[:user_id]
+    @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
