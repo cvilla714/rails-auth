310111965ca5bc318ad8f44b885f156e40111353
Cosmel Villalobos <cosmelvg@gmail.com>
2021-10-13 22:13:13 -0400

    Deleted unecessary code

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,16 +1,13 @@
 class ExpensesController < ApplicationController
   before_action :set_expense, only: %i[show edit update destroy]
-  # GET /expenses or /expenses.json
   def index
     @expenses = Expense.all
 
     render json: @expenses
   end
 
-  # GET /expenses/1 or /expenses/1.json
   def show; end
 
-  # GET /expenses/new
   def new
     @expense = Expense.new
   end
@@ -23,12 +20,9 @@ class ExpensesController < ApplicationController
     }
   end
 
-  # GET /expenses/1/edit
   def edit; end
 
-  # POST /expenses or /expenses.json
   def create
-    # @expense = Expense.new(expense_params)
     @expense = Expense.new
     @expense.user_id = Current.user.id if Current.user
     @expense.title = expense_params[:title]
@@ -37,16 +31,15 @@ class ExpensesController < ApplicationController
 
     respond_to do |format|
       if @expense.save
-        # format.html { redirect_to @expense, notice: "Expense was successfully created." }
+
         format.json { render :show, status: :created, location: @expense }
       else
-        # format.html { render :new, status: :unprocessable_entity }
+
         format.json { render json: @expense.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /expenses/1 or /expenses/1.json
   def update
     @expense.user_id = Current.user.id if Current.user
     @expense.title = expense_params[:title]
@@ -54,33 +47,27 @@ class ExpensesController < ApplicationController
     @expense.date = expense_params[:date]
     respond_to do |format|
       if @expense.save
-        # if @expense.update(expense_params)
-        # format.html { redirect_to @expense, notice: "Expense was successfully updated." }
         format.json { render :show, status: :ok, location: @expense }
       else
-        # format.html { render :edit, status: :unprocessable_entity }
+
         format.json { render json: @expense.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /expenses/1 or /expenses/1.json
   def destroy
     @expense.destroy
     respond_to do |format|
-      # format.html { redirect_to expenses_url, notice: "Expense was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_expense
     @expense = Expense.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def expense_params
     params.require(:expense).permit(:title, :amount, :date, :user_id, :created_at, :updated_at)
   end
